b17d2ac720475f99a8a2ea865e06e9538e22e18a
itsfsl <136988579+itsfsl@users.noreply.github.com>
2024-02-20 18:42:38 +0530

    Initial commit

---
+++ helloWorld/main.py
@@ -0,0 +1,11 @@
+print('Hello World!')
+
+name = 'Faisal'
+age = 28
+height = 5.5
+male = True
+
+name, age, height, male = 'Michael', 24, 5.7, True
+
+print('Hello ' + name + ', you are ' + str(age) + ' years old and your height is '
+      + str(height) + 'ft.')
